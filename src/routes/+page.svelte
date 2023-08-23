c5282331f3a564db55a8782fbcbeb56a1f2cef99
xuananle <lexuanan18102004@gmail.com>
2023-08-23 23:43:05 +0700

    Add tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button class="bg-indigo-600 px-4 py-3 text-center text-sm font-semibold inline-block text-white cursor-pointer uppercase transition duration-200 ease-in-out rou">
+    Tailwind Button
+  </button>
+
+<input type = "text" class = "text-5xl text-center text-red-500" placeholder = "Enter your name">
