8bcf88385e093638cfc32e479cd3e18e9c86c195
xuananle <lexuanan18102004@gmail.com>
2023-08-27 16:29:43 +0700

    Adding Animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    // import { userData } from "$lib/firebase";
+
+  </script>
+  
+  <svelte:head>
+    <title>Kung.foo - a place for links</title>
+    <meta name="description" content="Demo project for the Full SvelteKit Course by Fireship" />
+  </svelte:head>
+  
+  <main class="flex w-full min-h-screen">
+  
+    <div class="hero bg-base-200">
+      <div class="hero-content text-center">
+        <div class="w-full">
+          <h1 class="text-7xl font-bold">Bio-Shower </h1>
+          <p class="py-6 text-2xl">One simple link to share your entire developer portfolio</p>
+          <!-- {#if $userData?.username}
+            <a href="/{$userData.username}/edit" class="btn btn-primary">Edit Profile</a>
+          {:else} -->
+            <a href="/login" class="btn btn-primary">Get Started</a>
+            <!-- <a href="/fireship" class="btn btn-secondary">Example Profile</a>
+          {/if} -->
+        </div>
+      </div>
+    </div>
+    
+  </main>
+  
+  
